2cda4a0ae77a5a7583b3cb2f8399dd0eb4859493
Антон <tonikrus@rambler.ru>
2021-03-11 14:53:13 +0300

    Добавьте файлы проекта.

---
+++ Hw1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("Text.txt"))
+                {
+                    //File.SetAttributes(sr, FileAttributes.Normal);
+                    TextHandler.TextHandler textHandler1 = new TextHandler.TextHandler();
+                    TextHandler.TextHandler textHandler2 = new TextHandler.TextHandler();
+                    string text = sr.ReadToEnd();
+                    Stopwatch stopWatch1 = new Stopwatch();
+                    stopWatch1.Start();
+                    Dictionary<string, int> dict1 = textHandler1.SingleThreadedResult(text);
+                    stopWatch1.Stop();
+                    TimeSpan ts = stopWatch1.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds);
+                    Console.WriteLine("Время однопоточной реализации " + elapsedTime);
+                    Stopwatch stopWatch2 = new Stopwatch();
+                    stopWatch2.Start();
+                    Dictionary<string, int> dict2 = textHandler2.MultiThreadedResult(text);
+
+                    
+                    Directory.CreateDirectory("D:\\Temp");
+                    using (StreamWriter sw1 = new StreamWriter("D:\\Temp\\Answer1.txt", false))
+                    {
+                        foreach (string key in dict1.Keys)
+                        {
+                            sw1.WriteLine(key + " " + dict1[key]);
+                        }
+                    }
+                    using (StreamWriter sw2 = new StreamWriter("D:\\Temp\\Answer2.txt", false))
+                    {
+                        Dictionary<string, int> emptydict = new Dictionary<string, int>();
+                        while(emptydict == dict2)
+                        {
+                            System.Threading.Thread.Sleep(1);
+                        }
+                        stopWatch2.Stop();
+                        TimeSpan nts = stopWatch2.Elapsed;
+                        string nelapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds);
+                        Console.WriteLine("Время многопоточной реализации " + elapsedTime);
+                        foreach (string key in dict2.Keys)
+                        {
+                            sw2.WriteLine(key + " " + dict2[key]);
+                        }
+                        
+                    }
+                    Console.WriteLine("Успешное выполнение. Ответ лежит по адресу D:\\Temp");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TextHandler/TextHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace TextHandler
+{
+    public class TextHandler
+    {
+        private string[] words;
+        private Dictionary<string, int> dict = new Dictionary<string, int>();
+        public TextHandler()
+        {
+            dict = new Dictionary<string, int>();
+        }
+        private void Count(object input)
+        {
+            try
+            {
+                string text = ' ' + (string)input;
+                words = text.Split(new[] { '-', '.', '?', '!', ')', '(', ',', ':', ' ', '\"', '«', '»' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    if (!String.IsNullOrWhiteSpace(word))
+                    {
+                        if (dict.Keys.Contains(word.ToLower()))
+                        {
+                            dict[word.ToLower()]++;
+                        }
+                        else
+                        {
+                            dict.Add(word.ToLower(), 1);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+        }
+        public Dictionary<string, int> MultiThreadedResult(string text)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(Count));
+            
+            thread.Start(text);
+            if (thread.IsAlive)
+            {
+                thread.Join();
+            }
+            return dict;
+
+        }
+        public Dictionary<string, int> SingleThreadedResult(string text)
+        {
+            Count(text);
+            return dict;
+        }
+    }
+}
