39447b19f195f511183afeb3adf81d2b0628f322
Антон <tonikrus@rambler.ru>
2021-03-12 19:56:13 +0300

    Fix

--- Hw1/Program.cs
+++ Hw1/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
+using System.Reflection;
 
 namespace Hw1
 {
@@ -14,46 +15,55 @@ namespace Hw1
             {
                 using (StreamReader sr = new StreamReader("Text.txt"))
                 {
-                    //File.SetAttributes(sr, FileAttributes.Normal);
-                    TextHandler.TextHandler textHandler1 = new TextHandler.TextHandler();
-                    TextHandler.TextHandler textHandler2 = new TextHandler.TextHandler();
                     string text = sr.ReadToEnd();
-                    Stopwatch stopWatch1 = new Stopwatch();
-                    stopWatch1.Start();
-                    Dictionary<string, int> dict1 = textHandler1.SingleThreadedResult(text);
-                    stopWatch1.Stop();
-                    TimeSpan ts = stopWatch1.Elapsed;
-                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds);
-                    Console.WriteLine("Время однопоточной реализации " + elapsedTime);
+                    Assembly asm = Assembly.LoadFrom("TextHandler.dll");
+                    Type[] types = asm.GetTypes();
+
+                    Type t = asm.GetType("TextHandler.TextHandler", true, true);
+                    object obj1 = Activator.CreateInstance(t);
+                    object obj2 = Activator.CreateInstance(t);
+                    MethodInfo MultiThreadedResultmethod = t.GetMethod("MultiThreadedResult");
+                    MethodInfo SingleThreadedResultmethod = t.GetMethod("SingleThreadedResult");
+                    
                     Stopwatch stopWatch2 = new Stopwatch();
                     stopWatch2.Start();
-                    Dictionary<string, int> dict2 = textHandler2.MultiThreadedResult(text);
 
-                    
+                    object obj = SingleThreadedResultmethod.Invoke(obj2, new object[] { text });
+
+                    stopWatch2.Stop();
+                    TimeSpan ts = stopWatch2.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds);
+                    Console.WriteLine("Время однопоточной реализации " + elapsedTime);
+
+                    ConcurrentDictionary<string, int> dict2 = (ConcurrentDictionary<string, int>)obj;
+
+                    Stopwatch stopWatch1 = new Stopwatch();
+                    stopWatch1.Start();
+
+                    obj = MultiThreadedResultmethod.Invoke(obj1, new object[] { text });
+
+                    stopWatch1.Stop();
+                    ts = stopWatch1.Elapsed;
+                    elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds);
+                    Console.WriteLine("Время многопоточной реализации " + elapsedTime);
+
+                    ConcurrentDictionary<string, int> dict = (ConcurrentDictionary<string, int>)obj;
+
                     Directory.CreateDirectory("D:\\Temp");
                     using (StreamWriter sw1 = new StreamWriter("D:\\Temp\\Answer1.txt", false))
                     {
-                        foreach (string key in dict1.Keys)
+                        foreach (var key in dict.Keys)
                         {
-                            sw1.WriteLine(key + " " + dict1[key]);
+                            sw1.WriteLine(key + " " + dict[key]);
                         }
                     }
                     using (StreamWriter sw2 = new StreamWriter("D:\\Temp\\Answer2.txt", false))
                     {
-                        Dictionary<string, int> emptydict = new Dictionary<string, int>();
-                        while(emptydict == dict2)
-                        {
-                            System.Threading.Thread.Sleep(1);
-                        }
-                        stopWatch2.Stop();
-                        TimeSpan nts = stopWatch2.Elapsed;
-                        string nelapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Minutes, ts.Seconds, ts.Milliseconds);
-                        Console.WriteLine("Время многопоточной реализации " + elapsedTime);
-                        foreach (string key in dict2.Keys)
+                        foreach (var key in dict2.Keys)
                         {
                             sw2.WriteLine(key + " " + dict2[key]);
                         }
-                        
+
                     }
                     Console.WriteLine("Успешное выполнение. Ответ лежит по адресу D:\\Temp");
                 }
@@ -63,6 +73,7 @@ namespace Hw1
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
+            Console.WriteLine("Нажмите Enter для выхода");
             Console.ReadLine();
         }
     }
--- TextHandler/TextHandler.cs
+++ TextHandler/TextHandler.cs
@@ -1,60 +1,51 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace TextHandler
 {
     public class TextHandler
     {
-        private string[] words;
-        private Dictionary<string, int> dict = new Dictionary<string, int>();
+        private ConcurrentDictionary<string, int> dict = new ConcurrentDictionary<string, int>();
         public TextHandler()
         {
-            dict = new Dictionary<string, int>();
-        }
-        private void Count(object input)
-        {
-            try
-            {
-                string text = ' ' + (string)input;
-                words = text.Split(new[] { '-', '.', '?', '!', ')', '(', ',', ':', ' ', '\"', '«', '»' }, StringSplitOptions.RemoveEmptyEntries);
-                foreach (var word in words)
-                {
-                    if (!String.IsNullOrWhiteSpace(word))
-                    {
-                        if (dict.Keys.Contains(word.ToLower()))
-                        {
-                            dict[word.ToLower()]++;
-                        }
-                        else
-                        {
-                            dict.Add(word.ToLower(), 1);
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Ошибка: " + e.Message);
-            }
-        }
-        public Dictionary<string, int> MultiThreadedResult(string text)
-        {
-            Thread thread = new Thread(new ParameterizedThreadStart(Count));
-            
-            thread.Start(text);
-            if (thread.IsAlive)
-            {
-                thread.Join();
-            }
-            return dict;
 
         }
-        public Dictionary<string, int> SingleThreadedResult(string text)
+        private void AddWord(object input)
         {
-            Count(text);
+            string word = (string)input;
+            if (!String.IsNullOrWhiteSpace(word))
+            {
+                
+                if (!dict.TryAdd(word.ToLower(), 1))
+                {
+                    dict[word.ToLower()]++;       
+                }
+            }
+        }            
+    
+        public ConcurrentDictionary<string, int> MultiThreadedResult(string text)
+        {
+            string[] words = text.Split(new[] { '-', '.', '?', '!', ')', '(', ',', ':', ' ', '\"', '«', '»' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach(var word in words)
+            {
+                Thread thread = new Thread(new ParameterizedThreadStart(AddWord));
+                thread.Start(word);
+            }
+            return dict;
+        }
+
+        public ConcurrentDictionary<string, int> SingleThreadedResult(string text)
+        {
+            string[] words = text.Split(new[] { '-', '.', '?', '!', ')', '(', ',', ':', ' ', '\"', '«', '»' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach(var word in words)
+            {
+                AddWord(word);
+            }
             return dict;
         }
     }
